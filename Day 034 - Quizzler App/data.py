a9ee1a31cc4ccad94f12533ec3c00d38c734e514
Fares Alhomsi <90144763+FaresHomsi@users.noreply.github.com>
2022-05-11 18:39:54 +0200

    Add files via upload

---
+++ Day 034 - Quizzler App/data.py
@@ -0,0 +1,14 @@
+# Code TO Bring Data From API
+
+import requests
+parameters = {
+    "amount": 10,
+    "type": "boolean",
+}
+
+response = requests.get(url="https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
+data = response.json()
+
+
+question_data = data["results"]
